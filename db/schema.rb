a3fbbc9399ee4be0bcbba0176589cfcca38e78f0
zee <kooo9671354@gmail.com>
2019-06-26 17:25:26 +0800

    implement wage part

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_091949) do
+ActiveRecord::Schema.define(version: 2019_06_26_092228) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "wage_upper_bound"
+    t.integer "wage_lower_bound"
+    t.string "contact_email"
+    t.string "company"
+    t.string "city"
   end
 
   create_table "users", force: :cascade do |t|
